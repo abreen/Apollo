ece10f0bacfca820ded45e85fe6f955f44690de8
Alexander Breen <abreen@bu.edu>
2015-09-14 11:45:37 -0400

    Updated minor version number

--- lib/init.php
+++ lib/init.php
@@ -1,6 +1,6 @@
 <?php // 5.3.3
 
-define('APOLLO_VERSION', '2.0');
+define('APOLLO_VERSION', '2.1');
 
 // path to the Apollo installation
 // note: this will work as long as this file (init.php) is in the lib directory
